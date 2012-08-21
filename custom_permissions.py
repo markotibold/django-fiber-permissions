349c530014ddeb1684fe50ed73b1f1b193702d92
Marko Tibold <mtibold@leukeleu.nl>
2012-08-21 09:59:42 +0200

    The monkeys have left the building.

--- custom_permissions.py
+++ custom_permissions.py
@@ -1,4 +1,4 @@
-from guardian.shortcuts import get_objects_for_user, get_perms
+from guardian.shortcuts import get_objects_for_user, get_perms, assign
 
 from fiber.permissions import Permissions
 from fiber.models import Page
@@ -8,13 +8,14 @@ CONTENTITEM_PERMISSIONS = ('change_contentitem', 'delete_contentitem')
 
 
 class CustomPermissions(Permissions):
-    def filter_pages(self, user, qs):
+    def filter_objects(self, user, qs):
         if user.is_superuser:
             return qs
-        qs = qs.filter(id__in=get_objects_for_user(user, 'change_page', Page))
+        qs = qs.filter(id__in=get_objects_for_user(user, 'change_%s' % qs.model.__name__.lower(), Page))
         return qs
 
     def can_edit_content_item(self, user, content_item):
+        print content_item, get_perms(user, content_item)
         return 'change_contentitem' in get_perms(user, content_item)
 
     def can_edit_page(self, user, page):
@@ -27,3 +28,12 @@ class CustomPermissions(Permissions):
         if user.is_superuser:
             return qs
         return qs.filter(id__in=get_objects_for_user(user, CONTENTITEM_PERMISSIONS, qs))
+
+    def object_created(self, user, obj):
+        print user, obj, 'change_%s' % obj.__class__.__name__.lower()
+        assign('change_%s' % obj.__class__.__name__.lower(), user, obj)
+        import pdb
+        pdb.set_trace()
+
+
+# Todo merge with fiber Permissions class
--- urls.py
+++ urls.py
@@ -4,56 +4,6 @@ from django.conf import settings
 from django.contrib import admin
 
 
-from fiber.rest_api import views
-from fiber.admin import FiberAdminPageAdmin
-from fiber.models import Page
-from fiber.fiber_admin import site
-from guardian.shortcuts import get_objects_for_user, assign
-
-
-# Monkey patching
-views.ListView.guardian_permissions = None
-
-
-# This is where the real monkey patching happens\
-class PermissionModelAdmin(FiberAdminPageAdmin):
-
-    def save_model(self, request, obj, form, change):
-        """
-        Given a model instance save it to the database.
-        """
-        super(PermissionModelAdmin, self).save_model(request, obj, form, change)
-        assign('change_page', request.user, obj)
-
-site.unregister(Page)
-site.register(Page, PermissionModelAdmin)
-
-
-def post(self, request, *args, **kwargs):
-    response = super(views.ListView, self).post(request, *args, **kwargs)
-    for permission in self.guardian_permissions:
-        assign(permission, request.user, response.raw_content)  # raw_content is the Model instance
-    return response
-views.ListView.post = post
-
-
-def get_queryset_listview(self, *args, **kwargs):
-    qs = super(views.PaginatedListView, self).get_queryset(*args, **kwargs)
-    qs = qs.filter(id__in=get_objects_for_user(self.request.user, self.guardian_permissions, qs))
-    return qs
-views.PaginatedListView.get_queryset = get_queryset_listview
-
-
-def get_queryset_tree_listview(self, *args, **kwargs):
-    qs = super(views.TreeListView, self).get_queryset(*args, **kwargs)
-    qs = qs.filter(id__in=get_objects_for_user(self.request.user, self.guardian_permissions, qs))
-    return qs
-views.TreeListView.get_queryset = get_queryset_tree_listview
-
-views.FileListView.guardian_permissions = ('add_file', 'change_file', 'delete_file')
-views.ImageListView.guardian_permissions = ('add_image', 'change_image', 'delete_image')
-
-
 admin.autodiscover()
 
 
