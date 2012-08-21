67d7a3739dd474d9110343d668b226d7fe3b9d5f
Marko Tibold <mtibold@leukeleu.nl>
2012-08-21 10:16:50 +0200

    determine the model dynamically when filtering.

--- custom_permissions.py
+++ custom_permissions.py
@@ -1,7 +1,7 @@
 from guardian.shortcuts import get_objects_for_user, get_perms, assign
 
 from fiber.permissions import Permissions
-from fiber.models import Page
+
 
 PAGE_PERMISSIONS = ('change_page', 'delete_page')
 CONTENTITEM_PERMISSIONS = ('change_contentitem', 'delete_contentitem')
@@ -11,11 +11,10 @@ class CustomPermissions(Permissions):
     def filter_objects(self, user, qs):
         if user.is_superuser:
             return qs
-        qs = qs.filter(id__in=get_objects_for_user(user, 'change_%s' % qs.model.__name__.lower(), Page))
+        qs = qs.filter(id__in=get_objects_for_user(user, 'change_%s' % qs.model.__name__.lower(), qs.model))
         return qs
 
     def can_edit_content_item(self, user, content_item):
-        print content_item, get_perms(user, content_item)
         return 'change_contentitem' in get_perms(user, content_item)
 
     def can_edit_page(self, user, page):
@@ -30,10 +29,7 @@ class CustomPermissions(Permissions):
         return qs.filter(id__in=get_objects_for_user(user, CONTENTITEM_PERMISSIONS, qs))
 
     def object_created(self, user, obj):
-        print user, obj, 'change_%s' % obj.__class__.__name__.lower()
         assign('change_%s' % obj.__class__.__name__.lower(), user, obj)
-        import pdb
-        pdb.set_trace()
 
 
 # Todo merge with fiber Permissions class
